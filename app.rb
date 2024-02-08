fffee80f6a0b6ae40e13b2e0f2eaa7ba88a0d6df
HugoJendeby-NtiJohanneberg <119052994+HugoJendeby-NtiJohanneberg@users.noreply.github.com>
2024-02-08 16:02:47 +0100

    lektion 2

---
+++ app.rb
@@ -0,0 +1,66 @@
+class App < Sinatra::Base
+
+    def db
+        if @db == nil
+            @db = SQLite3::Database.new('./db/db.sqlite')
+            @db.results_as_hash = true
+        end
+        return @db
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    get '/movies' do
+        @movies = db.execute('SELECT * FROM movies')
+        erb :movies
+    end
+
+    get '/movies/new' do 
+        erb :'new'
+    end
+
+    post '/movies/' do 
+        name = params['name']
+        desc = params['description']
+        query = 'INSERT INTO movies (name, description) VALUES (?,?) RETURNING *'
+        result = db.execute(query, name, desc).first 
+        redirect "/movies/#{result['id']}" 
+    end
+    
+    get '/movies/:id/edit' do |id| 
+        @movies = db.execute('SELECT * FROM movies WHERE id = ?', id.to_i).first
+        erb :edit
+    end 
+
+    # Update 
+    #  get hämtar länk, Post skickar information till länk
+    # Post var formuläret skickar datan.
+    post '/movies/:id/update' do |id| 
+        kitten = params['content']
+        db.execute('UPDATE movies SET (content = ?) WHERE id = ?', kitten, id)
+        redirect "/movies/#{id}" 
+    end
+
+    # Get hämtar i detta fall delete-formuläret
+    get '/movies/:id/delete' do |id|
+        @kitten = db.execute('SELECT * FROM movies WHERE id=?;', id.to_i).first
+        erb :delete
+    end
+
+    # Post är dit formuläret skickar datorn,och där kommandot för att ta bort instansen körs.
+    post '/movies/:id/delete' do |id| 
+        db.execute('DELETE FROM movies WHERE id = ?', id)
+        redirect "/movies"
+    end
+
+    get '/movies/:id' do |kitten_id|
+        @movies_selected = db.execute('SELECT * FROM movies WHERE id=?;', kitten_id.to_i).first
+        erb :show
+    end
+
+
+
+    
+end
---
+++ db/seed.rb
@@ -0,0 +1,42 @@
+require 'sqlite3'
+
+def db
+    if @db == nil
+        @db = SQLite3::Database.new('./db/db.sqlite')
+        @db.results_as_hash = true
+    end
+    return @db
+end
+
+def drop_tables
+    db.execute('DROP TABLE IF EXISTS movies')
+end
+
+def create_tables
+
+    db.execute('CREATE TABLE movies(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        description TEXT
+    )')
+
+end
+
+def seed_tables
+
+    movies = [
+        {name: 'Pear', description: 'a sweet, juicy, yellow or green kitten with a round base and slightly pointed top'},
+        {name: 'Ape', description: 'a round, edible kitten having a red, green, or yellow skin'},
+        {name: 'Ba', description: 'a long, curved kitten with a usually yellow skin and soft, sweet flesh inside'},
+        {name: 'Oe', description: 'a round, orange-colored kitten that is valued mainly for its sweet juice'}
+    ]
+
+    movies.each do |movie|
+        db.execute('INSERT INTO movies (name, description) VALUES (?,?)', movie[:name], movie[:description])
+    end
+
+end
+
+drop_tables
+create_tables
+seed_tables
